2fe4caf7182181a44c006db57d3ffbb1877b8b6f
Rahul jha <RAHUL23082001JHA@gmail.com>
2024-05-18 18:47:39 +0530

    "same as last one"

--- Src/db_connector.py
+++ Src/db_connector.py
@@ -1,10 +1,11 @@
-from utils.logger import logging
-from utils.Exception import CustomException
+from logger import logging
+from Exception import CustomException
 import pymongo
 import json
 from datetime import datetime
 import os
 from dotenv import load_dotenv
+
 
 class DB():
 
@@ -13,7 +14,7 @@ class DB():
         load_dotenv()
         MONGO_DB =   os.getenv("MONGO_DB")    
         try:
-            myclient = pymongo.MongoClient(MONGO_DB)
+            self.myclient = pymongo.MongoClient(MONGO_DB)
             logging.info(f"DB connected at {MONGO_DB}")
         except Exception as e:
             raise CustomException(e)
@@ -23,8 +24,15 @@ class DB():
         collection = self.myclient[db][document]
         _ = collection.insert_one(data)
         logging.info(f"Data pushed into DB with following ids{_}")
-    def pull(self,filter:dict,colunm:dict,db:str,document:str) -> list:
+    def pull(self,db:str,document:str,filter:dict,colunm:dict) -> list:
         return list(self.myclient[db][document].find(filter=filter,projection = colunm))
-    
+    def push_many(self,data:list,db,document) -> None:
+        collection = self.myclient[db][document]
+        _ = collection.insert_many(data)
+        logging.info(f"Data pushed into DB with following ids{_.inserted_ids}")
 
     
+if __name__ == "__main__":
+    D = DB()
+    dic = {"id":123,"priority":1}
+    D.push(dic,"Clean","To_scan")
